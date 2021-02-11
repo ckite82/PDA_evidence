d25ba904f7cd42852aba9e27e9889423fbe034dc
Chris Kite <chris.kat82@googlemail.com>
2021-02-11 10:08:22 +0000

    task 1 complete

---
+++ I.T.6 dictionaries/players.py
@@ -0,0 +1,4 @@
+class Players:
+
+def get_quarter_back_name(self):
+    return quarter_backs["Green Bay"]
---
+++ I.T.6 dictionaries/run_tests.py
@@ -0,0 +1,5 @@
+from src.players import *
+from tests.players_test import *
+
+if __name__ == '__main__':
+    unittest.main()
